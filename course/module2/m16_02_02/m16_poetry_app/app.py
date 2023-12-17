5d8e207ffbf82c68d45833fec5b740e1a5b300a9
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2023-12-17 17:46:03 +0200

    added files to module2

--- course/module2/m16_02_02/m16_poetry_app/app.py
+++ course/module2/m16_02_02/m16_poetry_app/app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1 style='color:crimson'>Hello World!</h1>"
+    return ("<h1 style='color:crimson'>Hello World!</h1>\n"
+            "lorem ipsum dolor sit amet, consectetur")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3000, debug=True)
