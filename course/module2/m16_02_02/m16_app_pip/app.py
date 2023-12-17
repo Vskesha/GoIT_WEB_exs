019e6be15632a210ea967434df79e6c9ca7e1def
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2023-12-17 17:41:28 +0200

    added files to module2

--- course/module2/m16_02_02/m16_app_pip/app.py
+++ course/module2/m16_02_02/m16_app_pip/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello World!</h1>"
+    return ("<h1>Hello World!</h1>\n"
+            "lorem ipsum dolor sit amet, consectetur")
 
 
 if __name__ == '__main__':
