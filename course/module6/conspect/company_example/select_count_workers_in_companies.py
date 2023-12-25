e8a59c0f1afc0e11d919f9b7d9a3b2b9b2f88596
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2023-12-25 23:19:43 +0200

    added 15 files to module6

---
+++ course/module6/conspect/company_example/select_count_workers_in_companies.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+
+def execute_query(sql: str) -> list:
+    with sqlite3.connect("salary.db") as con:
+        cur = con.cursor()
+        cur.execute(sql)
+        return cur.fetchall()
+
+
+if __name__ == "__main__":
+    sql = """
+    SELECT COUNT(*), c.company_name
+    FROM employees e
+    LEFT JOIN companies c ON e.company_id = c.id
+    GROUP BY c.id;
+    """
+
+    print(*execute_query(sql), sep="\n")
