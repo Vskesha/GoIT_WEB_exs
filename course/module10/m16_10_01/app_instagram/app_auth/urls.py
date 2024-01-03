9eef6a7e58f3122bcf0d4719faf214a072f44b67
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-01-03 12:14:41 +0200

    added /app_auth/urls.py

---
+++ course/module10/m16_10_01/app_instagram/app_auth/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from . import views
+from .forms import LoginForm
+
+app_name = 'app_auth'
+
+urlpatterns = [
+    path('signup/', views.RegisterView.as_view(), name='signup'),
+    path('signin/', LoginView.as_view(template_name='app_auth/login.html', form_class=LoginForm), name='signin'),
+    path('logout/', LogoutView.as_view(template_name='app_auth/logout.html'), name='logout'),
+]
