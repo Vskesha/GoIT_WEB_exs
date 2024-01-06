44ba7aaed0badc431c2c09557ec1ea9e194dfeb8
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-01-06 16:35:26 +0200

    added authentication

--- course/module11/conspect/lesson2/main.py
+++ course/module11/conspect/lesson2/main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 
-from src.routes import notes, tags
+from src.routes import notes, tags, auth
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix='/api')
 app.include_router(tags.router, prefix='/api')
 app.include_router(notes.router, prefix='/api')
 
--- course/module11/conspect/lesson2/src/database/models.py
+++ course/module11/conspect/lesson2/src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, func, Table
+from sqlalchemy import Column, Integer, String, Boolean, func, Table, UniqueConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import DateTime
@@ -23,9 +23,27 @@ class Note(Base):
     description = Column(String(150), nullable=False)
     done = Column(Boolean, default=False)
     tags = relationship("Tag", secondary=note_m2m_tag, backref="notes")
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="notes")
 
 
 class Tag(Base):
     __tablename__ = "tags"
+    __table_args__ = (
+        UniqueConstraint('name', 'user_id', name='unique_tag_user'),
+    )
     id = Column(Integer, primary_key=True)
-    name = Column(String(25), nullable=False, unique=True)
+    name = Column(String(25), nullable=False)
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="tags")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
